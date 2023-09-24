2b74d1298e9ad37ac96d25f895f33e2d8b81ea36
shilcode <2850542@gmail.com>
2023-09-24 19:00:05 +0200

    test headed

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,8 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    baseURL:"https://www.epam.com/"
+    baseURL:"https://www.epam.com/",
+    headless:false
   },
   
 
